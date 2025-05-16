66175adf5ee829c75313112301e7ba52fc3d9335
rajesh chaudhari <93049758+rajeshchau@users.noreply.github.com>
2025-05-16 11:31:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+  event.preventDefault(); // prevent form reload
+  
+  // Accessing form using its ID
+  let form = document.getElementById("form1");
 
+  // Trimming extra spaces and getting values
+  let fname = form.fname.value.trim();
+  let lname = form.lname.value.trim();
+
+  // Alerting full name
+  alert(`${fname} ${lname}`);
 }
